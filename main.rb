df22b35f9386497a8efecbc7287de933e3905c24
Aimal Amiri <meaimal@hotmail.com>
2022-07-21 15:20:05 +0430

    Add basic actions list

--- main.rb
+++ main.rb
@@ -1,18 +1,53 @@
 require_relative './classes/person'
+require_relative './classes/student'
+require_relative './classes/'
 require_relative './classes/book'
 require_relative './classes/rental'
 require_relative './classes/capitalize_decorator'
 require_relative './classes/trimmer_decorator'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+class App
+  def run
+    actions.each { |key, value| puts "#{key}: #{value}" }
+    action = gets.chomp.to_i
+    action(action)
+  end
 
-book = Book.new('The Hobbit', 'J.R.R. Tolkien')
-rental = Rental.new('2018-01-01', book, person)
+  def actions
+    {
+      1 => 'List all books',
+      2 => 'List all people',
+      3 => 'Create a person',
+      4 => 'Create a book',
+      5 => 'Create a rental',
+      6 => 'List all rentals for a given person id',
+      7 => 'Exit'
+    }
+  end
 
-puts book.rentals.first.person.age
-puts rental.book.author
+  def action(action)
+    case action
+    when 1
+      puts 'list all books'
+    when 2
+      puts 'list all people'
+    when 3
+      puts 'create a person'
+    when 4
+      puts 'create a book'
+    when 5
+      puts 'create a rental'
+    when 6
+      puts 'list all rentals for a given person id'
+    else
+      puts 'Invalid action'
+    end
+  end
+end
+
+def main
+  app = App.new
+  app.run
+end
+
+main
