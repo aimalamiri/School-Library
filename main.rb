bce03f271fbd5887eccaef5e1aa4ced6329f1d01
Aimal Amiri <meaimal@hotmail.com>
2022-07-21 15:36:17 +0430

    Add create person section

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require_relative './classes/person'
 require_relative './classes/student'
-require_relative './classes/'
+require_relative './classes/teacher'
 require_relative './classes/book'
 require_relative './classes/rental'
 require_relative './classes/capitalize_decorator'
@@ -32,7 +32,7 @@ class App
     when 2
       puts 'list all people'
     when 3
-      puts 'create a person'
+      add_person
     when 4
       puts 'create a book'
     when 5
@@ -42,6 +42,39 @@ class App
     else
       puts 'Invalid action'
     end
+  end
+
+  def add_person
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    type = gets.chomp.to_i
+
+    puts 'Invalide input' if type != 1 && type != 2
+    add_student if type == 1 # Create a student if the user entered 1
+    add_teacher if type == 2 # Create a teacher if the user entered 2
+
+    run
+  end
+
+  def add_student
+    print 'Enter the name of the student: '
+    name = gets.chomp.to_s
+    print 'Enter the age of the student: '
+    age = gets.chomp.to_i
+    print 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp.to_s.downcase == 'y'
+    student = Student.new(age, name, parent_permission: parent_permission)
+    puts "Student with id #{student.id} created"
+  end
+
+  def add_teacher
+    print 'Enter the name of the teacher: '
+    name = gets.chomp.to_s
+    print 'Enter the age of the teacher: '
+    age = gets.chomp.to_i
+    print "And teacher's specialization: "
+    specilization = gets.chomp.to_s
+    teacher = Teacher.new(specilization, age, name)
+    puts "Student with id #{teacher.id} created"
   end
 end
 
