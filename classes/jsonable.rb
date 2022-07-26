7e6e49f78ec8099a2c7ec04cd3c8c29a2f469a58
Aimal Amiri <meaimal@hotmail.com>
2022-07-26 17:41:53 +0430

    Add jsonable class

---
+++ classes/jsonable.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+module JSONable
+  def to_json(*_args)
+    hash = {}
+    instance_variables.each do |var|
+      hash[var] = instance_variable_get var
+    end
+    hash.to_json
+  end
+
+  def from_json!(string)
+    JSON.load(string).each do |var, val|
+      instance_variable_set var, val
+    end
+  end
+end
